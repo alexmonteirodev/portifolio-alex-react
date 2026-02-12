ece073f459ed56fafeb09b768e98a84eb39d2ea8
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2026-02-12 12:21:03 +0100

    Update App.tsx

--- portifolio-alex-react/src/App.tsx
+++ portifolio-alex-react/src/App.tsx
@@ -1,9 +1,29 @@
 function App() {
   return (
     <>
-      <p className="read-the-docs bg-blue-200">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="flex flex-col items-center justify-center gap-3 px-96">
+        {/* section 1 - apresentation */}
+        <section className="flex flex-col gap-3 mt-20">
+          <h1 className="font-bold text-3xl">Hey, I'm Alex.</h1>
+          <p className="font-sans text-2xl text-gray-800 font-light leading-10 not-italic">
+            I’m a 27 year old Mobile Developer with experience in frontend
+            development for iOS applications and backend integration using
+            Supabase and SQL. <br />
+            I work across both interface and data layers, building structured,
+            scalable solutions rather than just screens. With a strong interest
+            in software architecture and system design, I continuously study and
+            refine my skills to grow as a Software Engineer. <br />
+            Currently based in Spain, I’m open to collaborating with teams where
+            I can contribute, learn, and help build meaningful digital products.
+          </p>
+          <span className="bg-black px-8 py-4 rounded-xl flex items-center justify-center self-start">
+            <a href="#" className="italic text-2xl text-white">
+              Say Hello!
+            </a>
+          </span>
+        </section>
+        <p>stacks</p>
+      </div>
     </>
   );
 }
