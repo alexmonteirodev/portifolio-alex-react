114c9edf21987e323e6ef4a90c5d07bb0b0f2e2b
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2026-03-02 16:47:27 +0100

    Update MockupGroup.tsx

--- portifolio-alex-react/src/components/MockupGroup.tsx
+++ portifolio-alex-react/src/components/MockupGroup.tsx
@@ -10,9 +10,9 @@ const MockupGroup = ({
   const alt = "mockup img";
   return (
     <div className="flex flex-row items-center justify-center gap-3">
-      <img className="w-full max-w-2xl h-auto" src={img1} alt={alt} />
-      <img className="w-full max-w-2xl h-auto" src={img2} alt={alt} />
-      <img className="w-full max-w-2xl h-auto" src={img3} alt={alt} />
+      <img className="w-full max-w-xs h-auto" src={img1} alt={alt} />
+      <img className="w-full max-w-xs h-auto" src={img2} alt={alt} />
+      <img className="w-full max-w-xs h-auto" src={img3} alt={alt} />
     </div>
   );
 };
