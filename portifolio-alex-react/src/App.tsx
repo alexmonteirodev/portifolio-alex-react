b333a43e0c36508143503fd431f8d40a17798db0
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2026-03-03 20:31:24 +0100

    scroll to top, bug bottom page

--- portifolio-alex-react/src/App.tsx
+++ portifolio-alex-react/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import SpinUpCase from "./pages/spinup";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/spinup" element={<SpinUpCase />} />
